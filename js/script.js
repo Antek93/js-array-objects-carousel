8582f5207a0d2916dcf8626e33321a531ff2ac2e
Antek93 <antonio.savino1993@gmail.com>
2023-01-19 15:52:40 +0100

    ff

--- js/script.js
+++ js/script.js
@@ -187,8 +187,6 @@ previousArrow.addEventListener('click',
 // BONUS 1:
 // Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l'immagine corrispondente.
 
-// BONUS 2:
-// Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l'immagine attiva dovrà cambiare alla successiva.
 
 // BONUS 3:
 // Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
